45b280328d5d11ede3f0c5f27be5bf41523890d2
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-01-18 07:56:52 -0300

    Configurando a conexão com servidor gmail

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,14 @@
 import org.junit.Test;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+
 
 public class AppTest {
 
-    private String userName = "eduardodevjavaweb@gmail.com";
+    private String username = "eduardodevjavaweb@gmail.com";
     private String senha = "dydqzvkgqaiytfsb";
 
     @Test
@@ -18,5 +22,15 @@ public class AppTest {
         properties.put("mail.smtp.port", "465");
         properties.put("mail.smtp.socketFactory.port", "465");
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, senha);
+            }
+
+        });
+
     }
 }
